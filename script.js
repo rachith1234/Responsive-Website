1090232febb56cdaabea2c15371817f2baf0f6b1
rachith1234 <35251263+rachith1234@users.noreply.github.com>
2023-04-18 14:32:58 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Javascript of responsive navigation menu
+
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", ()=>{
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+
+});
+
+// Javascript for video slider navigation
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+
+var sliderNav = function(manual){
+    btns.forEach((btn) =>{
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) =>{
+       slide.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+}
+
+btns.forEach((btn, i)=>{ 
+    btn.addEventListener("click", ()=>{
+        sliderNav(i);
+    });
+});
